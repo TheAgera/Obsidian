c5e6b4ba62252d1a6e3946d07b8996f2d93f4cb1
TheAgera <132917917+TheAgera@users.noreply.github.com>
2023-08-22 16:11:03 -0400

    08-22-23

--- Work Code/proxmoxfilter.ps1
+++ Work Code/proxmoxfilter.ps1
@@ -1,130 +1,43 @@
-# Header for markdown table, if needed
-# $header = "Column1 | Column"
 
-# This is known as a nested loop (loop within a loop)
-
-# for ($i = 1; $i -le $rowCount; $i++) {
-#     Write-Host "Row ${i}:"
-#     for ($j = 1; $j -le $colCount; $j++) {
-#         $cellValue = $table.ListRows($i).Range.Cells(1, $j).Text
-#         Write-Host "        Column ${j}: $cellValue"
-#     }
-# }
-
-# $cellValue = $worksheet.Cells.Item(3, 3).Value()
-# Write-Host $cellValue
-
-# Write-Host $table.ListRows.Count
-
-# $rowCount = 75
-# $colCount = 11
-
-# # Add conditional text based on vmid value
-#      if ($vmid -eq "Y") {
-#         $markdown += "### VMID is Y! Special line of text here!`n"
-#      } 
-
-
-# ComObjects allows objects created in different languages to interact, in this example we are opening an instance of excel.
-$excelApp = New-Object -ComObject excel.application
-
-# Tells excel to not visibly open, however it will open if you check task manager
-$excelApp.Visible = $false
-
-# Source file path
-$workbook = $excelApp.Workbooks.Open("C:\Users\ksuess\Documents\Obsidian\Work Code\ProxMoxServerSummary.xlsx")
-
-# Grabs the sheet by index
-$worksheet = $workbook.Sheets.Item(1) 
-
-# Grabs the table name
-$table = $worksheet.ListObjects("Table3")
-
-# File path for data output
-$outputFilePath = "C:\Users\ksuess\Documents\Obsidian\Work Code\ProxMox Summary Template.md"
-
-# List all of the rows.
-$rows = $table.ListRows
-
-
-
-# This is a loop that will iterate through each row in the $rows collection.
-foreach ($row in $rows) {
-    # Inside the loop, the script is taking data from each cell in the current row. After one row, it will (2,X), working in the (Y, X) coordinates.
-    $vmid = $row.range.cells(1, 1).Text 
-    $name = $row.range.cells(1, 2).Text 
-    $description = $row.range.cells(1, 3).Text 
-    $os = $row.range.cells(1, 4).Text 
-    $ip = $row.range.cells(1, 5).Text 
-    $intfqdn = $row.range.cells(1, 6).Text 
-    $ports = $row.range.cells(1, 7).Text 
-    $extfqdn = $row.range.cells(1, 8).Text 
-    $portainer = $row.range.cells(1, 9).Text 
-    $webcnsl = $row.range.cells(1, 11).Text 
-    $online = $row.range.cells(1, 12).Text 
-
-    $portainerText = if ($portainer -eq "Y") {
-        $portainerText = "$ip:9000 or 9001 (Portainer Web Console)"
-    } else { "" }
-    
-    $webcnslText = if ($webcnsl -eq "Y") {
-        $webcnslText = "$ip+9090 (Cockpit Web Console)"
-    } else { "" }
-
-    if ($portainer -eq "Y") {
-        $portainerText = "$ip+:9000 or 9001 (Portainer Web Console)"
-    }
-
-    if ($webcnsl -eq "Y") {
-        $webcnslText = "$ip+9090 (Cockpit Web Console)"
-    }
-    # Build the markdown string
-    $markdown ="
-# $description
-###### $vmid | $name
-___
-## Network Information:
-- IP: $ip
-
-- InternalFQDN: $intfqdn"
-    
-if ($portainerText) { $markdown += "`n    $portainerText" }
-if ($webcnslText) { $markdown += "`n    $webcnslText" }
-
-$markdown += "
-- ExternalFQDN: $extfqdn    
-
-### Listening Ports
-- $ports
-"
-    Write-Host $markdown
-    Add-Content -Path $outputFilePath -Value $markdown
-}
-
-$workbook.Close()
-$excelApp.Quit()
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($excelApp) | Out-Null
-
-
+##########################################################################################################################################
 
 function BuildTableForHosts($ports) {
     # Header of the table
     $table = "| Port #        | Description   |`n| ------------- |:-------------:|"
     foreach ($port in $ports.Split(',')) {
+        # 
         # Use switch cases to add specific logic for each port
-        # One offs: 9235,9300, 8
+        # Unregistered ports: 9235,9300,8777, 8777, 8125, 8090, 8000, 7880,4711,4369,3333, 3000, 2302
         $description = switch ($port.Trim()) {
-            "22" { "SSH"}
+            "22" {"SSH"}
+            "22" {"SMTP"}
             "53" { "DNS"}
-            "80" { "HTTP Port" }
-            "443" { "HTTPS Port" }
+            "80" {"HTTP Port"}
+            "82" {"XFER"}
+            "111" {"ONC RPC"}
+            "139" {"NetBIOS"}
+            "389" {"LDAP"}
+            "443" {"HTTPS Port"}
+            "445" {"Microsoft-DS"}
+            "631" {"Unix Printing"}
+            "636" {"LDAPS"}
+            "1025" {"Microsoft RPC"}
+            "1027" {"IPv6-4 NAT"}
+            "2083" {"Secure RADIUS"}
+            "3001" {"Honeywell"}
+            "3306" {"MySQL"}
+            "5432" {"PostgreSQL"}
+            "7070" {"RTSP"}
+            "6379" {"Redis"}
             "9000" {"Portainer HTTP"}
             "9001" {"Portainer Agent"}
             "9090" {"Cockpit"}
             "9443" {"Portainer HTTPS"}
             "10050" {"Zabbix Agent"}
             "10055" {"Zabbix Agent"}
-            default { "Undocumented" }
+            "19999" {"DNP-Secure"}
+            "27017" {"MongoDB"}
+            default { "Unregistered" }
         }
         $table += "`n| $port | $description |"
     }
@@ -143,7 +56,7 @@ $outputFilePath = "C:\Users\ksuess\Documents\Obsidian\Work Code\ProxMox Summary 
 $rows = $table.ListRows
 
 foreach ($row in $rows) {
-    # ...
+    # Inside the loop, the script is taking data from each cell in the current row. After one row, it will (2,X), working in the (Y, X) coordinates.
     $vmid = $row.range.cells(1, 1).Text 
     $name = $row.range.cells(1, 2).Text 
     $description = $row.range.cells(1, 3).Text 
@@ -155,8 +68,10 @@ foreach ($row in $rows) {
     $portainer = $row.range.cells(1, 9).Text 
     $webcnsl = $row.range.cells(1, 11).Text 
 
-    $portainerText = if ($portainer -eq "Y") { "$ip:9000 or 9001 (Portainer Web Console)" } else { "" }
-    $webcnslText = if ($webcnsl -eq "Y") { "$ip+9090 (Cockpit Web Console)" } else { "" }
+    # $portainerText = if ($portainer -eq "Y") { "$ip:9000 or 9001 (Portainer Web Console)" } else { "" }
+    # $webcnslText = if ($webcnsl -eq "Y") { "$ip+9090 (Cockpit Web Console)" } else { "" }
+    # if ($portainerText) { $markdown += "`n    - $portainerText" }
+    # if ($webcnslText) { $markdown += "`n    - $webcnslText" }
 
     $portsTable = BuildTableForHosts $ports
 
@@ -167,12 +82,7 @@ foreach ($row in $rows) {
 ___
 ## Network Information:
 - IP: $ip
-- InternalFQDN: $intfqdn"
-
-    if ($portainerText) { $markdown += "`n    - $portainerText" }
-    if ($webcnslText) { $markdown += "`n    - $webcnslText" }
-
-    $markdown += "
+- InternalFQDN: $intfqdn
 - ExternalFQDN: $extfqdn    
 
 ### Listening Ports
