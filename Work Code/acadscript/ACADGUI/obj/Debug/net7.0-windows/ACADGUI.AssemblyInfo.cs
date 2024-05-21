534f0ce98f35fb7e262506eff30fc90359f91904
ksuess <ksuess@commercecontrols.com>
2024-05-21 12:32:44 -0400

    05-21-24

--- Work Code/acadscript/ACADGUI/obj/Debug/net7.0-windows/ACADGUI.AssemblyInfo.cs
+++ Work Code/acadscript/ACADGUI/obj/Debug/net7.0-windows/ACADGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ACADGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb70edca34a17b6c57b378898a56857cd025f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ACADGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ACADGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
